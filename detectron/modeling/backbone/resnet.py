d8279dc761c1e15fad94bdf51b110e0a7936eefd
gchou <gchou@princeton.edu>
2020-11-26 03:57:39 +0800

    no warnings, but layers incorrect

--- detectron/modeling/backbone/resnet.py
+++ detectron/modeling/backbone/resnet.py
@@ -532,6 +532,8 @@ def build_resnet_backbone(cfg, input_shape):
             stage_kargs["num_blocks"] = 1
             blocks = ResNet.make_stage(**stage_kargs)
             in_channels *=2
+            bottleneck_channels *= 2
+            out_channels *= 2
 
         stages.append(blocks)
 
