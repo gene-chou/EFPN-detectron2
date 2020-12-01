795d88d486a2e1bd63b0042972a7dda98a5ac0d8
gchou <gchou@princeton.edu>
2020-12-01 22:43:13 +0800

    start texture transfer

--- detectron/modeling/backbone/ftt.py
+++ detectron/modeling/backbone/ftt.py
@@ -52,21 +52,6 @@ def FTT_get_p3pr(p2, p3, out_channels, norm):
     content_extractor = create_convs(out_channels * 4)
     texture_extractor = create_convs(out_channels * 2)
 
-    # subpixelconv is replaced by nn.PixelShuffle 
-    # def sub_pixel_conv(x, r=2):
-    #     C, H, W = x.shape[1:]
-    #     print ("shape: ",x.shape)
-    #     # assert C == self.in_channels
-    #     output = torch.zeros((x.shape[0], int(C/r**2), r*H, r*W))
-
-    #     for bs in range(x.shape[0]):
-    #         for c in range(int(C/r**2)):
-    #             values = x[bs][c*r*r:(c+1)*r*r] #shape would be [4, height, width]
-    #             #output[bs][c][i:i + r][j:j + r] = torch.reshape(values, (r,r))
-    #             output[bs][c] = torch.reshape(values, (r,r))
-    #     return output
-
-
     def extractor_helper(extractor, x): # extractor is tuple of (conv2D, cov2D, int)
         def each_iter(x):
             out = extractor[0](x)
@@ -79,6 +64,9 @@ def FTT_get_p3pr(p2, p3, out_channels, norm):
             out = each_iter(out)
         return out
 
+    # Image Super-Resolution by Neural Texture Transfer (Zhang 2019)
+    # we need to half the number of channels as well so it's maintained at 256
+    def texture_extractor():
 
     bottom = p3
     bottom = channel_scaler(bottom)
@@ -94,12 +82,10 @@ def FTT_get_p3pr(p2, p3, out_channels, norm):
     top = torch.cat((bottom, top), axis=1)
     top = extractor_helper(texture_extractor, top)
     top = top[:,256:]
-    print("\np2 shape: ",top.shape,"\n")
 
     # Since top has double the original # of channels, we "cast" bottom
     # to the same shape, then add to get p3'
     #bottom = torch.cat((bottom, bottom), axis=1)
     result = bottom + top
-    print("\nftt final result shape: ",result.shape,"\n")
 
     return result
