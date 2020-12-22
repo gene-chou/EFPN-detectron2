310154ee8f0487d8b194ae8870365a30e4c2fdb8
Gene Chou <54369977+gene-chou@users.noreply.github.com>
2020-12-22 22:56:31 +0800

    training script update

--- frcnn_x101_efpn.py
+++ frcnn_x101_efpn.py
@@ -15,7 +15,7 @@ from detectron2.model_zoo import model_zoo
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
+#cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
 
 # resnext ref: https://medium.com/@14prakash/understanding-and-implementing-architectures-of-resnet-and-resnext-for-state-of-the-art-image-cc5d0adf648e
 cfg.MODEL.RESNETS.NUM_GROUPS = 32 # 1 ==> ResNet; > 1 ==> ResNeXt
@@ -33,11 +33,11 @@ cfg.MODEL.FPN.IN_FEATURES = ["res2", "res3", "res4", "res5", "res6"]
 
 cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = 128
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
-cfg.SOLVER.IMS_PER_BATCH = 16
+cfg.SOLVER.IMS_PER_BATCH = 4
 
-cfg.SOLVER.BASE_LR = 0.001
-cfg.SOLVER.MAX_ITER = 40000
-cfg.SOLVER.STEPS = (8000, ) # The iteration number to decrease learning rate by GAMMA (0.1)
+cfg.SOLVER.BASE_LR = 0.0001
+cfg.SOLVER.MAX_ITER = 100000
+cfg.SOLVER.STEPS = (20000, ) # The iteration number to decrease learning rate by GAMMA (0.1)
 
 
 trainer = DefaultTrainer(cfg)
